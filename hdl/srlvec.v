1e67971b5c994d8ced181adbe5212e78e8397e32
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2023-06-14 13:02:24 -0400

    update slrvec to use SRL16Es

--- hdl/srlvec.v
+++ hdl/srlvec.v
@@ -1,20 +1,36 @@
 // dumb vectorized SRL
-module srlvec #(parameter NBITS=8)( input clk,
-				    input 	       ce,
-				    input [5:0]        a,
-				    input [NBITS-1:0]  din,
-				    output [NBITS-1:0] dout);
+// now updated so it doesn't always instantiate SRL32s
+// NOTE: if SRL16s are used you only have 4 address bits!
+//
+// Why would you want to use SRL16s? Because they pack denser:
+// you can fit 2 bits in a LUT instead of just one. So generally
+// you should always want to use SRL16s.
+module srlvec #(parameter NBITS=8,
+		parameter USE_SRL16="TRUE",
+                localparam ADDR_BITS=(USE_SRL16=="TRUE") ? 4 : 5)
+   ( input clk,
+     input		   ce,
+     input [ADDR_BITS-1:0] a,
+     input [NBITS-1:0]	   din,
+     output [NBITS-1:0]	   dout);
    generate
-      genvar 					       i;
-
+      genvar		   i;
+      
       for (i=0;i<NBITS;i=i+1) begin : BL
-	 SRLC32E u_bv(.D(din[i]),
-		      .CE(ce),
-		      .CLK(clk),
-		      .A(a),
-		      .Q(dout[i]));
-      end
-   endgenerate
+	 if (USE_SRL16 != "TRUE") begin : S32
+	    SRLC32E u_bv(.D(din[i]),
+			 .CE(ce),
+			 .CLK(clk),
+			 .A(a),
+			 .Q(dout[i]));
+	 end else begin : S16
+	    SRL16E u_bv(.D(din[i]),
+			.CE(ce),
+			.CLK(clk),
+			.A(a),
+			.Q(dout[i]));
+	 end
+      endgenerate
 endmodule // srlvec
 
 	 
