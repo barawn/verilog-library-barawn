bf81c1a2989ecf708fe338b64ab69eebea659931
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2023-07-12 13:05:47 -0400

    fix case where wishbone_arbiter is pointlessly called with 1 master

--- hdl/wishbone_arbiter.v
+++ hdl/wishbone_arbiter.v
@@ -18,22 +18,36 @@ module wishbone_arbiter(
 	wire [NUM_MASTERS-1:0] grants;
 	wire [NUM_MASTERS-1:0] requests;
 	
-	reg [NUM_MASTERS-1:0] active_grant = {NUM_MASTERS{1'b0}};
+    wire [NUM_MASTERS-1:0] active_grant;
+//	reg [NUM_MASTERS-1:0] active_grant = {NUM_MASTERS{1'b0}};
 	wire any_active = |(active_grant & cyc_i);
 
+    // The arbiter fails on N=1, so we special case around it.
+    // Obviously the entire module reduces down to
+    // "gnt_o = cyc_i" at that point, but it's just there to
+    // support parameterization.
 	generate
 	genvar i;
-		for (i=0;i<NUM_MASTERS;i=i+1) begin : ACTIVE_GRANT_LOOP
-			always @(posedge clk_i) begin : ACTIVE_GRANT_LOGIC
-				if (rst_i) active_grant[i] <= 0;
-				else if (!cyc_i[i]) active_grant[i] <= 0;
-				else if (cyc_i[i] && grants[i]) active_grant[i] <= 1;
-			end
-			assign requests[i] = cyc_i[i] && !any_active;
-		end
+	    if (NUM_MASTERS>1) begin : MM
+		    for (i=0;i<NUM_MASTERS;i=i+1) begin : ACTIVE_GRANT_LOOP
+                reg active_grant_ff = 0;
+                assign active_grant[i] = active_grant_ff;
+                always @(posedge clk_i) begin : ACTIVE_GRANT_LOGIC
+                    if (rst_i) active_grant_ff <= 0;
+                    else if (!cyc_i[i]) active_grant_ff <= 0;
+                    else if (cyc_i[i] && grants[i]) active_grant_ff <= 1;
+                end
+                assign requests[i] = cyc_i[i] && !any_active;
+            end
+            round_robin_arbiter #(.N(NUM_MASTERS)) u_arbiter(.rst_n(!rst_i),.clk(clk_i),.req(requests),.grant(grants));                    
+        end else begin : SM
+            // sooo dumb
+            assign requests[0] = cyc_i[0];
+            assign grants[0] = requests[0];
+            assign active_grant[0] = grants[0];
+        end
 	endgenerate
 	
-	round_robin_arbiter #(.N(NUM_MASTERS)) u_arbiter(.rst_n(!rst_i),.clk(clk_i),.req(requests),.grant(grants));
 	
 	assign gnt_o = (active_grant | grants);
 	// clk cyc req gnt active_grant
