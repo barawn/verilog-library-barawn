bac46a183f12320ee4a295e140fae32fc2a4bd04
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-11-20 09:36:28 -0500

    add utility function for pre-place script

--- tclbits/utility.tcl
+++ tclbits/utility.tcl
@@ -61,6 +61,11 @@ proc set_post_implementation_init_tcl { pinitscr } {
     set_script $pinitscr "STEPS.INIT_DESIGN.TCL.POST" "utils_1" "impl_1"
 }
 
+# As above, for pre-place script
+proc set_pre_place_tcl { prplacescr } {
+    set_script $prplacescr "STEPS.PLACE_DESIGN.TCL.PRE" "utils_1" "impl_1"
+}
+
 # As above, for post-place script
 proc set_post_place_tcl { pplacescr } {
     set_script $pplacescr "STEPS.PLACE_DESIGN.TCL.POST" "utils_1" "impl_1"
