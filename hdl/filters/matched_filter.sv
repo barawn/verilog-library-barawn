a5f62488777c4cf84899689ff8b3ee04133a5575
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-06-23 19:58:09 -0400

    properly map saturation stuff

--- hdl/filters/matched_filter.sv
+++ hdl/filters/matched_filter.sv
@@ -294,10 +294,10 @@ module matched_filter #(parameter NBITS=12,
                 
                 if (saturation) begin
                     // this is always the same
-                    M_sat_and_scale[NBITS-1] <= M_sum[NBITS-1];
+                    M_sat_and_scale[NBITS-1] <= M_sum[4+NBITS-1];
                     // this is always the opposite: so it's either 011111111111
                     //                                          or 100000000000
-                    M_sat_and_scale[NBITS-2:0] <= ~M_sum[NBITS-1];
+                    M_sat_and_scale[NBITS-2:0] <= ~M_sum[4+NBITS-1];
                 end else begin
                     // FIGURE OUT ROUNDING!!!
                     M_sat_and_scale <= M_sum[4 +: NBITS];
