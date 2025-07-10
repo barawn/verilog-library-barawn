6c736b44728473c98406a8cc7200b4e9b52e7081
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-07-10 16:57:23 -0400

    handle rounding and output scaling

--- hdl/filters/fourtap_systolic_preadd.sv
+++ hdl/filters/fourtap_systolic_preadd.sv
@@ -7,8 +7,9 @@ module fourtap_systolic_preadd #(
 				  parameter ROUND = "FALSE",      //! round the final tap to output bits
 				  parameter USE_ADD = "FALSE",    //! use the add_i input to add a value
 				  parameter SCALE_ADD = 0,        //! number of bits to upshift the add_i input
+				  parameter SCALE_OUT = 0,        //! number of bits to downshift the output
 				  parameter [1:0] ADD_INDEX = 0,  //! index to add the add_i input at 
-				  localparam NBITS = 12,          //! number of bits in input and output
+				  localparam NBITS = 12,          //! number of bits in input and (output-1)
 				  parameter CLKTYPE = "NONE")     //! clocktype for magic clock crossing
 	  ( input	  clk_i,             //! filter clock
 	    input     rst_i,             //! force DSPs into reset
@@ -20,6 +21,7 @@ module fourtap_systolic_preadd #(
 	    input [17:0]  coeff2_i,      //! coefficient of third DSP
 	    input [17:0]  coeff3_i,      //! coefficient of fourth DSP
 	    input [47:0]  pc_i,          //! cascade input if CASCADE == "TRUE"
+	    output [NBITS:0] dat_o,      //! data output
 	    output [47:0] p_o,           //! output of last DSP
 	    input [47:0]  pc_o           //! cascade output of last DSP
       );
@@ -47,19 +49,43 @@ module fourtap_systolic_preadd #(
    localparam USE_C1 = (USE_ADD == "TRUE") && ADD_INDEX == 2'd1 ? "TRUE" : "FALSE";
    localparam USE_C2 = (USE_ADD == "TRUE") && ADD_INDEX == 2'd2 ? "TRUE" : "FALSE";
    localparam USE_C3 = (USE_ADD == "TRUE") && ADD_INDEX == 2'd3 ? "TRUE" : "FALSE";
-      
+   
+   localparam USE_RND_2 = ((USE_ADD == "TRUE") && (ADD_INDEX == 2'd3)) ? ROUND : "FALSE";
+   localparam USE_RND_3 = ((USE_ADD != "TRUE") || (ADD_INDEX != 2'd3)) ? ROUND : "FALSE";
+
+
+   // Our output bits have (26-NBITS) useless bits at the bottom.
+   // And then we move the decimal point SCALE_OUT up
+   // For convergent rounding we need (26-NBITS)+SCALE_OUT-1 ones in the bottom.
+   localparam CONVERGENT_RND_ONES = (26-NBITS)+SCALE_OUT-1;
+   localparam [47:0] RND_CONST = {
+        {(48-CONVERGENT_RND_ONES){1'b0}},
+        {CONVERGENT_RND_ONES{1'b1}}
+   };
+   localparam USE_PATTERN_3 = (ROUND == "TRUE") ? "TRUE" : "FALSE";
+   // Pattern to be mached can just be all zeros
+   localparam [47:0] PATTERN_3 = {48{1'b0}};
+   localparam CONVERGENT_RND_MASK_ONES = 48-((26-NBITS)+SCALE_OUT);
+   localparam [47:0] MASK_3 = { {CONVERGENT_RND_MASK_ONES{1'b1}},
+                                {(48-CONVERGENT_RND_MASK_ONES){1'b0}} };
+         
    // The cross map means
    // sample 0 has a systolic 7 and feeds in 1
+   // I seriously hope this thing's not an idiot and can remap and merge
+   // SRLs as needed. We'll see. If it's dumb we'll push the delay outside
+   // and convert everything into like two SRLs or something.
    reg [NBITS-1:0]	  chainA_store0 = {NBITS{1'b0}};
    reg [NBITS-1:0]	  chainA_store1 = {NBITS{1'b0}};
+   reg [NBITS-1:0]    chainA_store2 = {NBITS{1'b0}};
    always @(posedge clk_i) begin : CA
       chainA_store0 <= preadd_i;
       chainA_store1 <= chainA_store0;
+      chainA_store2 <= chainA_store1;
    end
    
    // 30 bit input 27 bit preadder and save a bit so no saturation
    wire [29:0] dat_pad = { {4{dat_i[NBITS-1]}}, dat_i, { (30-NBITS-4) {1'b0}} };
-   wire [26:0] preadd_pad = { chainA_store1[NBITS-1], chainA_store1, {(27-NBITS-1){1'b0}} };
+   wire [26:0] preadd_pad = { chainA_store2[NBITS-1], chainA_store2, {(27-NBITS-1){1'b0}} };
    
    // Each sample is organized as the combination of
    // 2 4-tap systolic filters.
@@ -104,6 +130,7 @@ module fourtap_systolic_preadd #(
           u_dsp0( .clk_i(clk_i),
           .rst_i(rst_i),
 		  .a_i(dat_pad),
+		  .c_i(add_pad),
 		  .d_i(preadd_pad),
 		  .b_i(coeff0_i),
 		  .acout_o(a0_to_a1),
@@ -126,6 +153,7 @@ module fourtap_systolic_preadd #(
           .rst_i(rst_i),
 		  .acin_i(a0_to_a1),
 		  .pcin_i(dsp0_to_dsp1),
+		  .c_i(add_pad),
 		  .d_i(preadd_pad),
 		  .b_i(coeff1_i),
 		  .pcout_o(dsp1_to_dsp2),
@@ -135,6 +163,8 @@ module fourtap_systolic_preadd #(
 		  .USE_ACIN("TRUE"),
 		  .USE_ACOUT("TRUE"),
 		  .USE_D("TRUE"),
+		  .USE_ROUND(USE_RND_2),
+		  .RND_VAL(RND_CONST),
 		  .PREADD_REG(1),
 		  .MULT_REG(1),
 		  .AREG(1),
@@ -146,16 +176,24 @@ module fourtap_systolic_preadd #(
           .rst_i(rst_i),
 		  .acin_i(a1_to_a2),
 		  .pcin_i(dsp1_to_dsp2),
+		  .c_i(add_pad),
 		  .d_i(preadd_pad),
 		  .b_i(coeff2_i),
 		  .pcout_o(dsp2_to_dsp3),
 		  .acout_o(a2_to_a3));
    // last core doesn't use the cascade path		  
+   wire patdet;
    fir_dsp_core #(.ADD_PCIN("TRUE"),
 		  .USE_C(USE_C3),
 		  .USE_ACIN("TRUE"),
 		  .USE_ACOUT("FALSE"),
 		  .USE_D("TRUE"),
+		  .USE_RND(USE_RND_3),
+		  .RND_VAL(RND_CONST),
+		  .USE_PATTERN(USE_PATTERN_3),
+		  .PATTERN_VAL(PATTERN_3),
+		  .MASK_VAL(MASK_3),
+		  .USE_CARRYIN("TRUE"),
 		  .PREADD_REG(1),
 		  .MULT_REG(1),
 		  .AREG(1),
@@ -166,9 +204,17 @@ module fourtap_systolic_preadd #(
           .rst_i(rst_i),
 		  .acin_i(a2_to_a3),
 		  .pcin_i(dsp2_to_dsp3),
+		  .c_i(add_pad),
 		  .d_i(preadd_pad),
 		  .b_i(coeff3_i),
+		  .carryin_i(1'b1),
 		  .p_o(p_o),
+		  .pattern_o(patdet),
 		  .pcout_o(pc_o)); 
+
+    // the LSB is normally at 26-NBITS, we scale up by SCALE_OUT
+    localparam LSB = 26-NBITS+SCALE_OUT;
+    assign dat_o[NBITS:1] = p_o[LSB+1 +: NBITS];
+    assign dat_o[0] = (ROUND == "TRUE") ? p_o[LSB] && !patdet : p_o[LSB];
 
 endmodule // fourtap_systolic_preadd
