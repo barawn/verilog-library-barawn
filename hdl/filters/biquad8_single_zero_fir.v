4e000a07c63cc63cd37c196a18da47bce870f048
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-10-13 15:20:24 -0400

    fix sample select to work with any number of samples

--- hdl/filters/biquad8_single_zero_fir.v
+++ hdl/filters/biquad8_single_zero_fir.v
@@ -69,8 +69,8 @@ module biquad8_single_zero_fir #(parameter NBITS=16,
       genvar		     i;
       for (i=0;i<NSAMP;i=i+1) begin : LP
 	 wire [NBITS-1:0] this_samp = dat_i[NBITS*i +: NBITS];
-	 wire [NBITS-1:0] last_samp = dat_i[NBITS*((i+7)%8) +: NBITS];
-	 wire [NBITS-1:0] llast_samp = dat_i[NBITS*((i+6)%8) +: NBITS];
+	 wire [NBITS-1:0] last_samp = dat_i[NBITS*((i+NSAMP-1)%NSAMP) +: NBITS];
+	 wire [NBITS-1:0] llast_samp = dat_i[NBITS*((i+NSAMP-2)%NSAMP) +: NBITS];
 
 	 // dsp1 handles this_samp and llast_samp
 	 // we add the preadder reg and possibly mreg
